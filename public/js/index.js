345777ea867cb803c850abab3d5485884333d1ab
Gabriel DP <gabrieldominguezprado@gmail.com>
2021-11-06 03:24:38 -0300

    Septimo Commit

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,16 @@
+// refresh ante navegación.
+
+window.addEventListener( "pageshow", ( event ) => {
+    const historyTraversal = event.persisted || 
+                           ( typeof window.performance != "undefined" && 
+                                window.performance.navigation.type === 2 );
+    if ( historyTraversal ) {
+      window.location.reload();
+    };
+});
+
+// Referencias Html
+
 const input = document.querySelector('input');
 
 const divAlert = document.querySelector('.alert');
